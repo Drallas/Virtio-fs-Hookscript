9f3011d904fce9c01dce4fd4f97eb107668dc858
Drallas <24792888+Drallas@users.noreply.github.com>
2023-11-01 11:32:27 +0100

    Update get-hook script.sh

--- get-hook script.sh
+++ get-hook script.sh
@@ -5,5 +5,3 @@ cd /var/lib/vz/snippets
 sudo sh -c "wget https://github.com/Drallas/Virtio-fs-Hookscript-/raw/main/Script/virtiofs-hook.pl"
 sudo sh -c "wget https://github.com/Drallas/Virtio-fs-Hookscript-/raw/main/Script/virtiofs_hook.conf"
 sudo chmod +x /var/lib/vz/snippets/virtiofs-hook.pl
-
-# test
