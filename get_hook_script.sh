f0347014a8e2a44b0f3664b4373af3ed1075c4d9
Drallas <24792888+Drallas@users.noreply.github.com>
2023-11-01 20:14:05 +0100

    rename

--- get_hook_script.sh
+++ get_hook_script.sh
@@ -2,6 +2,6 @@
 
 sudo mkdir -p /var/lib/vz/snippets
 cd /var/lib/vz/snippets
-sudo sh -c "wget https://github.com/Drallas/Virtio-fs-Hookscript-/raw/main/Script/virtiofs-hook.pl"
-sudo sh -c "wget https://github.com/Drallas/Virtio-fs-Hookscript-/raw/main/Script/virtiofs_hook.conf"
-sudo chmod +x /var/lib/vz/snippets/virtiofs-hook.pl
+sudo sh -c "wget https://github.com/Drallas/Virtio-fs-Hookscript/raw/main/Script/virtiofs_hook.pl"
+sudo sh -c "wget https://github.com/Drallas/Virtio-fs-Hookscript/raw/main/Script/virtiofs_hook.conf"
+sudo chmod +x /var/lib/vz/snippets/virtiofs_hook.pl
